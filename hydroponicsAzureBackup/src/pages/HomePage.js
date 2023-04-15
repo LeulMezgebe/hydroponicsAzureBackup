e58085ecd9474b9dfd3937b06b124f7ab43f1f1e
kevin r minn <52860121+kevinrjminn@users.noreply.github.com>
2023-04-14 21:18:50 -0400

    Add files via upload

---
+++ hydroponicsAzureBackup/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/HomePage.module.css";
+
+const HomePage = () => {
+    return (
+        <div className={styles.homePage}>
+            <h1>Welcome to the Hydroponic Monitoring System</h1>
+            <p>General information about the project goes here...</p>
+        </div>
+    );
+};
+
+export default HomePage;
