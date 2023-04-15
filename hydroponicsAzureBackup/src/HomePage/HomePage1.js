8449d27a0d41b7c1dedaf25ac794c8986ec5fbea
kevin r minn <52860121+kevinrjminn@users.noreply.github.com>
2023-04-14 21:16:57 -0400

    Add files via upload

---
+++ hydroponicsAzureBackup/src/HomePage/HomePage1.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { Container, Typography, Box } from '@mui/material';
+//
+// const HomePage = () => {
+//     return (
+//         <Container>
+//             <Box sx={{ mt: 4, mb: 4 }}>
+//                 <Typography variant="h2" align="center" gutterBottom>
+//                     Welcome to Our Hydroponic Monitoring Project
+//                 </Typography>
+//             </Box>
+//             <Box sx={{ textAlign: 'justify', mt: 4, mb: 4 }}>
+//                 <Typography variant="body1">
+//                     This project aims to provide a comprehensive and user-friendly dashboard for monitoring and managing hydroponic systems. With our intuitive interface, users can easily track various parameters such as temperature, humidity, pH levels, and nutrient levels in their hydroponic systems, ensuring optimal growth conditions for their plants.
+//                 </Typography>
+//                 <Typography variant="body1" sx={{ mt: 2 }}>
+//                     Our dashboard is designed with real-time data visualization in mind, allowing users to make informed decisions about their hydroponic systems quickly and efficiently. By combining cutting-edge technology with a deep understanding of hydroponic systems, we strive to make hydroponic farming more accessible and profitable for farmers around the world.
+//                 </Typography>
+//             </Box>
+//             {/* for more contents to be added */}
+//         </Container>
+//     );
+// };
+//
+// export default HomePage;
